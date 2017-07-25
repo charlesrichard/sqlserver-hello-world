72c9853bc50f34973dd70231a344e134d1167411
Chuck Svoboda <crsvoboda@gmail.com>
2017-07-25 13:10:01 -0400

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,8 @@
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 using System.Net;
  
 namespace ConsoleApplication {
@@ -7,14 +10,159 @@ namespace ConsoleApplication {
         public void Configure(IApplicationBuilder app){
             app.Run(context => {
                 //return context.Response.WriteAsync("Hello world");
-                return context.Response.WriteAsync("Yup, this is C#.Net, written by chuck!!" + 
-                       System.Environment.NewLine + 
-                       System.Environment.NewLine + 
-                       "Host name: " + Dns.GetHostName().ToString() + 
-                       System.Environment.NewLine + 
-                       System.Environment.NewLine +
-                       System.Environment.StackTrace);
+                return context.Response.WriteAsync();
             });
         }
+
+        private string CreateFooter()
+        {
+            return System.Environment.NewLine + 
+                "Yup, this is C#.Net, written by chuck!!" + 
+                System.Environment.NewLine + 
+                System.Environment.NewLine + 
+                "Host name: " + Dns.GetHostName().ToString() + 
+                System.Environment.NewLine + 
+                System.Environment.NewLine +
+                System.Environment.StackTrace;
+        }
     }
+
+    public class BaseDataAccess  
+    {  
+        protected string ConnectionString { get; set; }  
+        
+        public BaseDataAccess()  
+        {  
+        }  
+        
+        public BaseDataAccess(string connectionString)  
+        {  
+            this.ConnectionString = connectionString;  
+        }  
+        
+        private SqlConnection GetConnection()  
+        {  
+            SqlConnection connection = new SqlConnection(this.ConnectionString);  
+            if (connection.State != ConnectionState.Open)  
+                connection.Open();  
+            return connection;  
+        }  
+        
+        protected DbCommand GetCommand(DbConnection connection, string commandText, CommandType commandType)  
+        {  
+            SqlCommand command = new SqlCommand(commandText, connection as SqlConnection);  
+            command.CommandType = commandType;  
+            return command;  
+        }  
+        
+        protected SqlParameter GetParameter(string parameter, object value)  
+        {  
+            SqlParameter parameterObject = new SqlParameter(parameter, value != null ? value : DBNull.Value);  
+            parameterObject.Direction = ParameterDirection.Input;  
+            return parameterObject;  
+        }  
+        
+        protected SqlParameter GetParameterOut(string parameter, SqlDbType type, object value = null, ParameterDirection parameterDirection = ParameterDirection.InputOutput)  
+        {  
+            SqlParameter parameterObject = new SqlParameter(parameter, type); ;  
+        
+            if (type == SqlDbType.NVarChar || type == SqlDbType.VarChar || type == SqlDbType.NText || type == SqlDbType.Text)  
+            {  
+                parameterObject.Size = -1;  
+            }  
+        
+            parameterObject.Direction = parameterDirection;  
+        
+            if (value != null)  
+            {  
+                parameterObject.Value = value;  
+            }  
+            else  
+            {  
+                parameterObject.Value = DBNull.Value;  
+            }  
+        
+            return parameterObject;  
+        }  
+        
+        protected int ExecuteNonQuery(string procedureName, List<DbParameter> parameters, CommandType commandType = CommandType.StoredProcedure)  
+        {  
+            int returnValue = -1;  
+        
+            try  
+            {  
+                using (SqlConnection connection = this.GetConnection())  
+                {  
+                    DbCommand cmd = this.GetCommand(connection, procedureName, commandType);  
+        
+                    if (parameters != null && parameters.Count > 0)  
+                    {  
+                        cmd.Parameters.AddRange(parameters.ToArray());  
+                    }  
+        
+                    returnValue = cmd.ExecuteNonQuery();  
+                }  
+            }  
+            catch (Exception ex)  
+            {  
+                //LogException("Failed to ExecuteNonQuery for " + procedureName, ex, parameters);  
+                throw;  
+            }  
+        
+            return returnValue;  
+        }  
+        
+        protected object ExecuteScalar(string procedureName, List<SqlParameter> parameters)  
+        {  
+            object returnValue = null;  
+        
+            try  
+            {  
+                using (DbConnection connection = this.GetConnection())  
+                {  
+                    DbCommand cmd = this.GetCommand(connection, procedureName, CommandType.StoredProcedure);  
+        
+                    if (parameters != null && parameters.Count > 0)  
+                    {  
+                        cmd.Parameters.AddRange(parameters.ToArray());  
+                    }  
+        
+                    returnValue = cmd.ExecuteScalar();  
+                }  
+            }  
+            catch (Exception ex)  
+            {  
+                //LogException("Failed to ExecuteScalar for " + procedureName, ex, parameters);  
+                throw;  
+            }  
+        
+            return returnValue;  
+        }  
+        
+        protected DbDataReader GetDataReader(string procedureName, List<DbParameter> parameters, CommandType commandType = CommandType.StoredProcedure)  
+        {  
+            DbDataReader ds;  
+        
+            try  
+            {  
+                DbConnection connection = this.GetConnection();  
+                {  
+                    DbCommand cmd = this.GetCommand(connection, procedureName, commandType);  
+                    if (parameters != null && parameters.Count > 0)  
+                    {  
+                        cmd.Parameters.AddRange(parameters.ToArray());  
+                    }  
+        
+                    ds = cmd.ExecuteReader(CommandBehavior.CloseConnection);  
+                }  
+            }  
+            catch (Exception ex)  
+            {  
+                //LogException("Failed to GetDataReader for " + procedureName, ex, parameters);  
+                throw;  
+            }  
+        
+            return ds;  
+        }  
+    } 
 }
